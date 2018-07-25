75b4829c2bcf2a1402ae912ad17ef12f05b83756
--replace-all <matthieu.paret@lifen.fr>
2018-07-25 11:25:50 +0200

    fix types to default

--- src/sns-receiver.js
+++ src/sns-receiver.js
@@ -9,14 +9,14 @@ router.post('/:index/:types?', function (req, res, next) {
 	let index = req.params.index
 	let currentDate = new Date()
 	let dailyIndex = index + '-' + currentDate.getFullYear() + '.' + (currentDate.getMonth() + 1) + '.' + currentDate.getDate()
-	let types = req.params.types
-	console.log(`running /${index}/${types}`)
+	let environment = req.params.types
+	console.log(`running /${index}/${environment}`)
 //	Override types for ES 6.3 where an index can only handle a single type value
 //	Leaving the ability to specify a type in the route in order to allow for mapping it in the future into another field.
 //	if (!types) {
 //		types = 'default'
 //	}
-	types = 'default'
+	let types = 'default'
 	let body = req.body
 	if (body.Message) {
 		let raw = body.Message
