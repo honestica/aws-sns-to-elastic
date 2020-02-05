f139e8b9d59c3c6a7acdc7d2ed89a8fc8a257b6e
Marc Millien <marc.millien@lifen.fr>
2020-02-05 15:44:46 +0100

    [logs] Better logging output

--- src/sns-receiver.js
+++ src/sns-receiver.js
@@ -10,7 +10,6 @@ router.post('/:index/:types?', function (req, res, next) {
   let currentDate = new Date()
   let dailyIndex = index + '-' + currentDate.getFullYear() + '.' + (currentDate.getMonth() + 1) + '.' + currentDate.getDate()
   let environment = req.params.types
-  console.log(`running /${index}/${environment}`)
   //  Override types for ES 6.3 where an index can only handle a single type value
   //  Leaving the ability to specify a type in the route in order to allow for mapping it in the future into another field.
   //  if (!types) {
@@ -18,12 +17,13 @@ router.post('/:index/:types?', function (req, res, next) {
   //  }
   let types = 'default'
   let body = req.body
+  console.log(`Received message on /${index}/${environment} from ARN ${body.TopicArn}`)
   if (body.Message) {
     let raw = body.Message
     try {
       body.Message = JSON.parse(raw)
     } catch(e) {
-      console.log("Failed parsing body as JSON, falling back to raw")
+      console.log(`Failed parsing json: ${e}, falling back to raw`)
       body.Message = {}
       body.rawMessage = raw
     }
@@ -33,7 +33,7 @@ router.post('/:index/:types?', function (req, res, next) {
     .then((result) => {
       res.send(result)
     }).catch(e => {
-      console.log("Error = ", e)
+      console.log(`Error from ES: ${e}`)
       next(e)
     })
 })
